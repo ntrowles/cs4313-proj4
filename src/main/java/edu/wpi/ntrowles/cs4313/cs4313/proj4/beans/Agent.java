59b9c9b994d7544bb34c7c878109f68395549f9b
Nicholas Rowles <cpufourproject@gmail.com>
2016-10-09 21:15:15 -0400

    Do you hear that? That is the sound of inevitability...

---
+++ src/main/java/edu/wpi/ntrowles/cs4313/cs4313/proj4/beans/Agent.java
@@ -0,0 +1,21 @@
+package edu.wpi.ntrowles.cs4313.cs4313.proj4.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Agent {
+	private List<Path> history;
+	
+	public Action selectAction(State curState){
+		List<Action> validActions = new ArrayList<Action>();
+		//Add all valid actions
+		
+		//select best actions based on function
+		Action bestAction = new Action();
+		return bestAction;
+	}
+	
+	public double doAction(State curState){
+		Action nextAction = 
+	}
+}
