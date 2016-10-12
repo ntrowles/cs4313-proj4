5d62325d866207d013b71de45bbd11927230a469
bgsarkis <bgsarkis@gmail.com>
2016-10-12 19:31:30 -0400

    aaaa

--- src/test/java/edu/wpi/ntrowles/cs4313/cs4313/proj4/StateTest.java
+++ src/test/java/edu/wpi/ntrowles/cs4313/cs4313/proj4/StateTest.java
@@ -1,8 +1,21 @@
 package edu.wpi.ntrowles.cs4313.cs4313.proj4;
 
-public class StateTest {
+import org.junit.Assert.*;
+import org.junit.Test;
+import junit.framework.TestCase;
+import edu.wpi.ntrowles.cs4313.cs4313.proj4.beans.State;
+
+public class StateTest extends TestCase{
+	
 	@Test
 	public void toStringTest(){
+		State testState = new State();
 		
+		for(int r = 0; r < 10; r++){
+			for(int c = 0; c < 10; c++){
+				assertTrue(testState.percieve()[r][c] == 'h');
+			}
+			
+		}
 	}
 }
