d2d93b1b8bcab23326e71c4024632dc7a2f2ef07
Nicholas Rowles <cpufourproject@gmail.com>
2016-10-13 01:11:31 -0400

    updated main to test nextState fn

--- src/main/java/edu/wpi/ntrowles/cs4313/cs4313/proj4/Main.java
+++ src/main/java/edu/wpi/ntrowles/cs4313/cs4313/proj4/Main.java
@@ -1,6 +1,9 @@
 package edu.wpi.ntrowles.cs4313.cs4313.proj4;
 
+import edu.wpi.ntrowles.cs4313.cs4313.proj4.beans.Action;
 import edu.wpi.ntrowles.cs4313.cs4313.proj4.beans.Agent;
+import edu.wpi.ntrowles.cs4313.cs4313.proj4.beans.MoveType;
+import edu.wpi.ntrowles.cs4313.cs4313.proj4.beans.Position;
 import edu.wpi.ntrowles.cs4313.cs4313.proj4.beans.State;
 
 public class Main {
@@ -12,14 +15,16 @@ public class Main {
 		State curState = new State();
 //		curState.initializeBoard();
 		curState.toString();
-//		while(continuePlaying){
+		while(continuePlaying){
 			a.doAction(curState);
+			
+			curState.nextState(new Action(new Position((int)(Math.random()*curState.getMinesweeperBoard().length), (int)(Math.random()*curState.getMinesweeperBoard()[0].length)), MoveType.DIG));
 			
 			System.out.println(curState.toString());
 			if(curState.isTerminal()){
 				continuePlaying = false;
 			}
-//		}
+		}
 	
 	}
 	
